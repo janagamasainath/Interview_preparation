250c194c9d4888280e73aa796262b66348fe8043
janagamasainath <124761197+janagamasainath@users.noreply.github.com>
2024-07-10 17:11:19 +0530

    flatmap

--- src/map/MapandFlatMap.java
+++ src/map/MapandFlatMap.java
@@ -4,23 +4,18 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class MapandFlatMap {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Map<Integer, List<Integer>> map = new HashMap<>();
-		map.put(1,Arrays.asList(111,222,333,444,555,666));
-		map.put(2, Arrays.asList(1,2,3,4,5,6));
-		map.put(3, Arrays.asList(11,22,33,44,55,66));
-		for(Map.Entry<Integer, List<Integer>> mm: map.entrySet()) {
-			Integer key = mm.getKey();
-			List<Integer> value = mm.getValue();
-			System.out.println(mm);
-		}
+		List<List<Integer>> al=Arrays.asList(Arrays.asList(1,2,3),Arrays.asList(4,5,6),Arrays.asList(7,8,9));
 		
+		List<Integer> collect = al.stream().flatMap(List::stream).collect(Collectors.toList());
+		System.out.println(collect);
 		
-
 	}
 
 }
