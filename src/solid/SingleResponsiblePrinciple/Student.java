d6800f81a067002184f18090d9a098175f329701
janagamasainath <124761197+janagamasainath@users.noreply.github.com>
2024-07-10 20:19:32 +0530

    solid SingleResponsiblePrinciple

---
+++ src/solid/SingleResponsiblePrinciple/Student.java
@@ -0,0 +1,17 @@
+package solid.SingleResponsiblePrinciple;
+//this is not the perfect example for singleResponsiblePrinciple because student class is doing 3 jobs
+//i.e., addStudentDetails() , printStudentDetails(), calcutateStudentMarks()
+public class Student {
+	
+	public void addStudentDetails() {
+		
+	}
+
+	public void printStudentDetails() {
+		
+	}
+	
+	public void calcutateStudentMarks() {
+		
+	}
+}
---
+++ src/solid/SingleResponsiblePrinciple/StudentPrint.java
@@ -0,0 +1,9 @@
+package solid.SingleResponsiblePrinciple;
+//this is = the perfect example for singleResponsiblePrinciple because student class is doing 1 jobs
+//i.e.,  printStudentDetails()
+public class StudentPrint {
+	public void printStudentDetails() {
+		
+	}
+
+}
