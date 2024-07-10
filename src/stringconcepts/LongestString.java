8daa816c5354401083316e29ff4b29583a481fde
janagamasainath <124761197+janagamasainath@users.noreply.github.com>
2024-07-10 17:24:22 +0530

    longestString

--- src/stringconcepts/LongestString.java
+++ src/stringconcepts/LongestString.java
@@ -1,9 +1,15 @@
 package stringconcepts;
 
+import java.util.Arrays;
+
 public class LongestString {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String[] str= {"java","springboot","microservices","Angular","hibernate","restApi"};
+		
+		String longestString = Arrays.stream(str).reduce((w1,w2)->w1.length()>w2.length()?w1:w2).get();
+		System.out.println(longestString);
 
 	}
 
